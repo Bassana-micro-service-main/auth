a9df440f13672c783b59c5d9dbd7e29f7b6341d2
Bassana07 <120794432+Bassana07@users.noreply.github.com>
2026-02-09 08:28:15 +0000

    Initial commit

---
+++ src/test/java/com/bassana/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bassana.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
